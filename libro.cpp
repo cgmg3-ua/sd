8add10ed9631d321e35089a0f2961fce5fe1fd6f
Carlos Guirao <cgmg3@alu.ua.es>
2024-12-26 14:24:18 +0100

    prueba hook

--- libro.cpp
+++ libro.cpp
@@ -5,7 +5,7 @@ Libro::Libro(string t, string a, int anio) : titulo(t), autor(a), anioPublicacio
 
 // Método para mostrar los detalles del libro
 void Libro::mostrarDetalles() const {
-    cout << "Titulo: " << titulo
+    cout << "Titulo:络 " << titulo
          << ", Autor: " << autor
-         << ", Anyo: " << anioPublicacion << endl;
+         << ", Año: " << anioPublicacion << endl;
 }
